468ad65e412411c31eab266d467824cdc8cf8388
Roger Soh <jaggedfel2560@gmail.com>
2020-02-28 14:22:50 +0800

    add neo pixel ring and no battery check

---
+++ neo_pixel_nightrider_no_batt_check/neo_pixel_nightrider_no_batt_check.ino
@@ -0,0 +1,249 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PIN 6
+#define NumberPixel 20
+#define GROUP 20
+#define OFFSET 30
+#define BRIGHTNESS 12
+int printInterval = 2000;
+unsigned long printTime = 0;
+const float OnTime = 0.5 ; // OnTime in hours
+bool ON = true;
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NumberPixel, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (ON && millis() > OnTime * 3600000) {
+    ON = false;
+  }
+  if (ON) {
+    // Some example procedures showing how to display to the pixels:
+    nightrider(strip.Color(60, 0, 0), 40); // Red
+    nightriderFwd(strip.Color(0, 10, 0), 40); // Green
+    nightrider(strip.Color(0, 0, 10), 40); // Blue
+    //  colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
+    // Send a theater pixel chase in...
+    //  theaterChase(strip.Color(127, 127, 127), 50); // White
+    theaterChase(strip.Color(40, 0, 0), 100); // Red
+    theaterChase(strip.Color(10, 10, 0), 100); // Red n Green
+    rainbow(10);
+    theaterChase(strip.Color(10, 00, 10), 100); // Red n Blue
+    rainbowCycle(10);
+  } else {
+    for (int i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, 0);
+    }
+    strip.show();
+    delay(50);
+  }
+}
+
+// nightrider
+void nightrider(uint32_t c, uint8_t wait) {
+  for (uint16_t k = 0; k < NumberPixel / GROUP; k++) {
+    for (uint16_t j = 0; j < GROUP; j++) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + GROUP) {
+        strip.setPixelColor(i + j, 0);
+        if (j + 1 >= GROUP) {
+          strip.setPixelColor(i + j + 1 - GROUP, c / 10);
+        } else {
+          strip.setPixelColor(i + j + 1, c / 10);
+        }
+        if (j + 2 >= GROUP) {
+          strip.setPixelColor(i + j + 2 - GROUP, c / 2);
+        } else {
+          strip.setPixelColor(i + j + 2, c / 2);
+        }
+        if (j + 3 >= GROUP) {
+          strip.setPixelColor(i + j + 3 - GROUP, c);
+        } else {
+          strip.setPixelColor(i + j + 3, c);
+        }
+        if (j + 4 >= GROUP) {
+          strip.setPixelColor(i + j + 4 - GROUP, c / 2);
+        } else {
+          strip.setPixelColor(i + j + 4, c / 2);
+        }
+        if (j + 5 >= GROUP) {
+          strip.setPixelColor(i + j + 5 - GROUP, c / 10);
+        } else {
+          strip.setPixelColor(i + j + 5, c / 10);
+        }
+      }
+      strip.show();
+      delay(wait);
+    }//forward
+  }
+  for (uint16_t k = 0; k < NumberPixel / GROUP; k++) {
+    for (uint16_t j = GROUP; j > 0; j--) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + GROUP) {
+        strip.setPixelColor(i + j, 0);
+        if (j + 1 >= GROUP) {
+          strip.setPixelColor(i + j + 1 - GROUP, c / 10);
+        } else {
+          strip.setPixelColor(i + j + 1, c / 10);
+        }
+        if (j + 2 >= GROUP) {
+          strip.setPixelColor(i + j + 2 - GROUP, c / 2);
+        } else {
+          strip.setPixelColor(i + j + 2, c / 2);
+        }
+        if (j + 3 >= GROUP) {
+          strip.setPixelColor(i + j + 3 - GROUP, c);
+        } else {
+          strip.setPixelColor(i + j + 3, c);
+        }
+        if (j + 4 >= GROUP) {
+          strip.setPixelColor(i + j + 4 - GROUP, c / 2);
+        } else {
+          strip.setPixelColor(i + j + 4, c / 2);
+        }
+        if (j + 5 >= GROUP) {
+          strip.setPixelColor(i + j + 5 - GROUP, c / 10);
+        } else {
+          strip.setPixelColor(i + j + 5, c / 10);
+        }
+        if (j + 6 >= GROUP) {
+          strip.setPixelColor(i + j + 6 - GROUP, 0);
+        } else {
+          strip.setPixelColor(i + j + 6, 0);
+        }
+      }
+      strip.show();
+      delay(wait);
+    }//reverse
+  }//number of times
+}
+
+// nightriderFwd
+void nightriderFwd(uint32_t c, uint8_t wait) {
+  for (uint16_t k = 0; k < 2 * NumberPixel / GROUP; k++) {
+    for (uint16_t j = 0; j < GROUP; j++) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + GROUP) {
+        strip.setPixelColor(i + j, 0);
+        if (j + 1 >= GROUP) {
+          strip.setPixelColor(i + j + 1 - GROUP, c / 10);
+        } else {
+          strip.setPixelColor(i + j + 1, c / 10);
+        }
+        if (j + 2 >= GROUP) {
+          strip.setPixelColor(i + j + 2 - GROUP, c / 2);
+        } else {
+          strip.setPixelColor(i + j + 2, c / 2);
+        }
+        if (j + 3 >= GROUP) {
+          strip.setPixelColor(i + j + 3 - GROUP, c);
+        } else {
+          strip.setPixelColor(i + j + 3, c);
+        }
+        if (j + 4 >= GROUP) {
+          strip.setPixelColor(i + j + 4 - GROUP, c / 2);
+        } else {
+          strip.setPixelColor(i + j + 4, c / 2);
+        }
+        if (j + 5 >= GROUP) {
+          strip.setPixelColor(i + j + 5 - GROUP, c / 10);
+        } else {
+          strip.setPixelColor(i + j + 5, c / 10);
+        }
+      }
+      strip.show();
+      delay(wait);
+    }//forward
+  }//number of times
+}
+
+
+void rainbow(uint8_t wait) {
+  strip.setBrightness(BRIGHTNESS);
+  uint16_t i, j;
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+  strip.setBrightness(255);
+  for (i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, 0);
+  }
+  strip.show();
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  strip.setBrightness(BRIGHTNESS);
+  uint16_t i, j;
+  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+  strip.setBrightness(255);
+  for (i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, 0);
+  }
+  strip.show();
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+    for (int q = 0; q < 3; q++) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, c);  //turn every third pixel on
+      }
+      strip.show();
+      delay(wait);
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
---
+++ neo_pixel_ring_running_grp_of_4/neo_pixel_ring_running_grp_of_4.ino
@@ -0,0 +1,88 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PIN 6
+#define NumberPixel 20
+#define GROUP 5
+#define readVoltPin 12
+#define powerOnPin 4
+#define sensingPin A2
+#define lowBattVolt 3.8
+#define BRIGTNESS 50
+float batteryVolt = 0.0;
+int printInterval = 2000;
+unsigned long printTime = 0;
+const float OnTime = 0.5 ; // OnTime in hours
+bool ON = true;
+
+// Parameter 1 = number of pixels
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NumberPixel, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+
+  pixels.begin();
+  pixels.show(); // Initialize all pixels to 'off'
+
+  pinMode(readVoltPin, OUTPUT);
+  pinMode(powerOnPin, OUTPUT);
+  digitalWrite(readVoltPin, HIGH);
+  digitalWrite(powerOnPin, HIGH);
+  delay(50);
+  batteryVolt = analogRead(sensingPin);
+  batteryVolt = batteryVolt * 5 / 1024.0;
+  digitalWrite(readVoltPin, LOW);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (millis() - printTime > printInterval) {
+    printTime = millis();
+    digitalWrite(readVoltPin, HIGH);
+    delay(50);
+    batteryVolt = analogRead(sensingPin);
+    batteryVolt = batteryVolt * 5 / 1024.0;
+    Serial.print("Battery Volt:  ");
+    Serial.println(batteryVolt, 3);
+    digitalWrite(readVoltPin, LOW);
+//    if (batteryVolt > lowBattVolt) {
+//      ON = true;
+//    } else {
+//      ON = false;
+//    }
+  }
+  if (ON) {
+    for (int j = 0; j < GROUP; j++) {
+      for (int i = 0; i < NumberPixel / GROUP; i++) {
+        for (int k = 0; k < 3; k++) {
+          int m = j + i * GROUP + k;
+          if (m > NumberPixel - 1) {
+            m = m - NumberPixel;
+          }
+          pixels.setPixelColor(m, pixels.Color(BRIGTNESS, 0, 0));
+        }
+      }
+      pixels.show();
+      delay(200);
+      pixels.clear();
+    }
+  }
+}
